f7cdd7ce11fbeec05481b934bb2d94d9dfbca414
leonelveron <leonelveron.12@gmail.com>
2022-04-12 20:19:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+alert("Hola Mundo") /* esto es un alerta al abrir la pag */
+
+let alumno = "Leo"; /* creo la variable */
+
+const gobierno = "Ciudad de Bs As." /* constantes nunca cambian */
+
+console.log("el nombre del alumno es" +" " +alumno); /* imprimo en pantalla */
+
+
+/////////////// tipos de datos
+
+//////String van entre comillas "", '', `` con este ultimo te olvidas de concatenar el "+" y eso
+
+/* let algo= "es algo"
+
+ let nombre= "pedro"
+ 
+ let oracion= algo + " de "+ nombre*/
+
+
+
+//////// Number 
+
+/* let numero1= 2 (prompt tira un mensaje para que el usuario interactue seria dinamico la cosa)
+
+   let numero2= 4
+   
+   let suma= numero1+numero2
+   let resta= numero1-numero2
+   let mult= numero1*numero2
+   let div= numero1numero2
+
+   -----------------------------------
+
+   let num1 = prompt ("Ingrese el primer numero") ingresaria como string entonces hay que parsearlo para pasarlo a num el parseInt toma el entero, parseFloat te toma los decimales
+
+   let num2= prompt ("ingrese el segundo numero")
+
+   entonces quedaria
+
+   let num1= parseInt(prompt("Ingrese el primer numero"))
+
+   let num1= parseInt(prompt("Ingrese el segundo numero"))
+   
+   */
+
